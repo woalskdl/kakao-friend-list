1caf0cf297c9e7dcfd829989224a3ddf256d0c5a
wooljam <woalskdl@gmail.com>
2023-11-02 22:57:05 +0900

    FlatList 적용

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
@@ -23,29 +23,73 @@ export default function App() {
     setIsOpened(!isOpened);
   }
 
+  const ItemSeparatorComponent = () => <Margin height={13}/>;
+  const renderItem = ({item}) => (
+    <View>
+      <Profile
+        uri={item.uri}
+        name={item.name}
+        introduction={item.introduction}
+        isMe={false}
+      />
+    </View>
+  )
+
+  const ListHeaderComponent = () => (
+    <View style={{backgroundColor: 'white'}}>
+      <Header/>
+
+      <Margin height={10}/>
+
+      <Profile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+        isMe={true}
+      />
+
+      <Margin height={15}/>
+      <Division />
+      <Margin height={12}/>
+
+      <FriendSection 
+        friendProfileLen={friendProfiles.length}
+        onPressArrow={onPressArrow}
+        isOpened={isOpened}
+      />
+
+      <Margin height={5}/>
+    </View>
+  )
+
+  const ListFooterComponent = () => <Margin height={10}/>;
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={isOpened ? friendProfiles : []}
+        contentContainerStyle={{paddingHorizontal: 15}}
+        keyExtractor={(_, idx) => idx}  // 미사용 변수는 _로 표기
+        stickyHeaderIndices={[0]}
+        ItemSeparatorComponent={ItemSeparatorComponent}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+        ListFooterComponent={ListFooterComponent}
+        showsVerticalScrollIndicator={false}
+      />
+      <View>
+        <TabBar 
+          selectedTabIdx={selectedTabIdx}
+          setSelectedTabIdx={setSelectedTabIdx}
+        />
+      </View>
+    </View>
+  )
+
   return (
     <View style={styles.container}>
       <View style={{flex: 1, paddingHorizontal: 15}}>
-        <Header/>
-
-        <Margin height={10}/>
-
-        <Profile
-          uri={myProfile.uri}
-          name={myProfile.name}
-          introduction={myProfile.introduction}
-        />
         
-        <Margin height={15}/>
-        <Division />
-        <Margin height={12}/>
-        
-        <FriendSection 
-          friendProfileLen={friendProfiles.length}
-          onPressArrow={onPressArrow}
-          isOpened={isOpened}
-        />
-
         <FriendList
           data={friendProfiles}
           isOpened={isOpened}
@@ -53,10 +97,6 @@ export default function App() {
 
       </View>
 
-      <TabBar 
-        selectedTabIdx={selectedTabIdx}
-        setSelectedTabIdx={setSelectedTabIdx}
-      />
     </View>
   );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,14 +1,22 @@
 import { View, Text, Image } from "react-native"
 import Margin from "./Margin"
 
-export default ({name, introduction, uri}) => {
+export default ({name, introduction, uri, isMe}) => {
+  const size = isMe ? 60 : 50;
+
   return (
     <View style={{ flexDirection: 'row'}}>
-      <Image source={{uri: uri}} style={{width: 50, height: 50, borderRadius: 20}}/>
+      <Image source={{uri}} style={{width: size, height: size, borderRadius: size * 0.4}}/>
       <View style={{ justifyContent: 'center', marginLeft: 10}}>
-        <Text style={{fontWeight: 'bold', fontSize: 16}}>{name}</Text>
-        <Margin height={6}/>
-        <Text style={{fontSize: 12, color: 'grey'}}>{introduction}</Text>
+        <Text style={{fontWeight: isMe ? 'bold' : undefined, fontSize: isMe ? 16 : 15}}>{name}</Text>
+        {
+          introduction && (
+            <View>
+              <Margin height={isMe ? 6 : 2}/>
+              <Text style={{fontSize: isMe ? 12 : 11, color: 'grey'}}>{introduction}</Text>
+            </View>
+          )
+        }
       </View>
     </View>
     

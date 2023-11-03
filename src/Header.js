d70e5eb8c479757b058a0c7295842799eebcc7cd
wooljam <woalskdl@gmail.com>
2023-11-03 21:48:54 +0900

    버튼 터치범위 조절

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Ionicons } from '@expo/vector-icons'; 
 
 const IconButton = ({name}) => {
   return (
-    <View style={{paddingHorizontal: 6}}>
+    <TouchableOpacity hitSlop={{top: 15, bottom: 15}} style={{ paddingHorizontal: 6 }}>
       <Ionicons name={name} size={24} color="black" />
-    </View>
+    </TouchableOpacity>
   )
 }
 
